2e4e4a58e32ba0372cc287df796be49ef2f4653d
yeison\Yeison Fabian <yeisonramirez@wposs.com>
2024-09-28 20:01:10 -0500

    IMPLEMENTACION
    *) Se realiza la conexión con mi servidor para obtener los mensajes desde DB.
    *) Se realiza la creacion del modelo de mensajes.

--- lib/src/pages/chat_page.dart
+++ lib/src/pages/chat_page.dart
@@ -5,7 +5,11 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:chat_app/src/services/chat_service.dart';
+import 'package:chat_app/src/services/socket_service.dart';
+import 'package:chat_app/src/services/auth_service.dart';
 import 'package:chat_app/src/widgets/chat_message.dart';
+
+import 'package:chat_app/src/models/mensajes_response.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -17,12 +21,58 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
   final _focusNode = new FocusNode();
   bool _estaEscribiendo = false;
 
+  late ChatService chatService;
+  late SocketService socketService;
+  late AuthService authService;
+
   List<ChatMessage> _messages = [];
 
   @override
+  void initState() {
+    super.initState();
+
+    this.chatService = Provider.of<ChatService>(context, listen: false);
+    this.socketService = Provider.of<SocketService>(context, listen: false);
+    this.authService = Provider.of<AuthService>(context, listen: false);
+
+    this.socketService.socket.on('mensaje-personal', _escucharMensaje);
+    this._cargarHistorial(this.chatService.usuarioPara!.uid!);
+  }
+
+  void _cargarHistorial(String usuarioID) async {
+    List<Mensaje> chat = await this.chatService.getChat(usuarioID);
+    final historial = chat.map((m) => new ChatMessage(
+          texto: m.mensaje,
+          uid: m.de,
+          animationController: new AnimationController(
+              vsync: this, duration: Duration(milliseconds: 0))
+            ..forward(),
+        ));
+
+    setState(() {
+      _messages.insertAll(0, historial);
+    });
+  }
+
+  void _escucharMensaje(dynamic payload) {
+    ChatMessage message = new ChatMessage(
+        texto: payload['mensaje'],
+        uid: payload['de'],
+        animationController: AnimationController(
+          vsync: this,
+          duration: Duration(milliseconds: 300),
+        ));
+
+    setState(() {
+      _messages.insert(0, message);
+    });
+
+    message.animationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final chatService = Provider.of<ChatService>(context);
-    final usuarioPara = chatService.usuarioPara;
+    final usuarioPara = this.chatService.usuarioPara;
 
     return Scaffold(
       appBar: AppBar(
@@ -129,15 +179,15 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
     );
   }
 
-  _handleSubmit(String text) {
-    if (text.isEmpty) return;
+  _handleSubmit(String texto) {
+    if (texto.isEmpty || texto.trim().length == 0) return;
 
     _textController.clear();
     _focusNode.requestFocus();
 
     final newMessage = new ChatMessage(
-      uid: '123',
-      texto: text,
+      uid: this.authService.usuario!.uid!,
+      texto: texto,
       animationController: AnimationController(
         vsync: this,
         duration: Duration(milliseconds: 300),
@@ -149,14 +199,21 @@ class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
     setState(() {
       _estaEscribiendo = false;
     });
+
+    this.socketService.emit('mensaje-personal', {
+      'de': this.authService.usuario!.uid,
+      'para': this.chatService.usuarioPara!.uid,
+      'mensaje': texto
+    });
   }
 
   @override
   void dispose() {
-    // TODO:
     for (ChatMessage message in _messages) {
       message.animationController.dispose();
     }
+
+    this.socketService.socket.off('mensaje-personal');
     super.dispose();
   }
 }
--- lib/src/services/chat_service.dart
+++ lib/src/services/chat_service.dart
@@ -1,9 +1,22 @@
+import 'package:chat_app/src/models/mensajes_response.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
+import 'package:chat_app/src/global/environment.dart';
+import 'package:chat_app/src/services/auth_service.dart';
 import 'package:chat_app/src/models/usuario.dart';
 
 class ChatService with ChangeNotifier {
-  Usuario ?usuarioPara;
+  Usuario? usuarioPara;
 
+  Future<List<Mensaje>> getChat(String usuarioID) async {
+    final resp = await http
+        .get(Uri.parse('${Environment.apiUrl}/mensajes/$usuarioID'), headers: {
+      'Content-Type': 'application/json',
+      'x-token': await AuthService.getToken(),
+    });
+    final mensajesResp = mensajesResponseFromJson(resp.body);
 
+    return mensajesResp.mensajes;
+  }
 }
